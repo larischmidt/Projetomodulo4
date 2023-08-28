f1d088da90428079b7383ad8047c46fe53836c93
LarissaSchmidt <larissaschmidt541@gmail.com>
2023-08-28 16:07:28 -0300

    inclusão de comandos para ordenação do projeto

--- arquivo.js
+++ arquivo.js
@@ -1,1 +1,16 @@
-import 
+import readline from "readline-sync";
+
+const propriedadesCss = [];
+
+function comandosPrompt(){
+        let entrada = (readline.question('Qual a propriedade Css desejada? ou digite "SAIR" para encerrar: ')).toUpperCase();
+        if (entrada === 'SAIR'){
+    propriedadesCss.sort().forEach((propriedadesCss)=>{
+        console.log(propriedadesCss)
+    })
+    }else{
+        propriedadesCss.push(entrada)
+        comandosPrompt()
+    }
+}
+    comandosPrompt();
